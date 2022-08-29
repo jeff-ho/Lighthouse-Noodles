8e06fca1e40df996ed7acb5479b8925b19008618
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-29 20:19:12 +0000

    Rearrange Menu Components

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -10,11 +10,11 @@ $(document).ready(function () {
       $appetizerList.empty();
 
       for(const appetizer of response.items) {
-        $(`<h4 class="appetizer-name">`).text(appetizer.name).appendTo($appetizerList);
-        $(`<p class="appetizer-description">`).text(appetizer.description).appendTo($appetizerList);
         $(`<img src=${appetizer.img_url} class="item-image">`).appendTo($appetizerList);
+        $(`<i class="fa-solid fa-cart-plus"></i>`).appendTo($appetizerList);
+        $(`<h4 class="appetizer-name">`).text(appetizer.name).appendTo($appetizerList);
+        //$(`<p class="appetizer-description">`).text(appetizer.description).appendTo($appetizerList);
         $(`<p class="appetizer-price">`).text(`Item Price: $${appetizer.price/100}`).appendTo($appetizerList);
-
       }
     });
   }
@@ -30,11 +30,11 @@ $(document).ready(function () {
       $noodleList.empty();
 
       for(const noodle of response.items) {
-        $(`<h4 class="noodle-name">`).text(noodle.name).appendTo($noodleList);
-        $(`<p class="noodle-description">`).text(noodle.description).appendTo($noodleList);
         $(`<img src=${noodle.img_url} class="item-image">`).appendTo($noodleList);
+        $(`<i class="fa-solid fa-cart-plus"></i>`).appendTo($noodleList);
+        $(`<h4 class="noodle-name">`).text(noodle.name).appendTo($noodleList);
+        //$(`<p class="noodle-description">`).text(noodle.description).appendTo($noodleList);
         $(`<p class="noodle-price">`).text(`Item Price: $${(noodle.price)/100}`).appendTo($noodleList);
-
       }
     });
   }
@@ -50,11 +50,11 @@ $(document).ready(function () {
       $snackList.empty();
 
       for(const snack of response.items) {
-        $(`<h4 class="snack-name">`).text(snack.name).appendTo($snackList);
-        $(`<p class="snack-description">`).text(snack.description).appendTo($snackList);
         $(`<img src=${snack.img_url} class="item-image">`).appendTo($snackList);
+        $(`<i class="fa-solid fa-cart-plus"></i>`).appendTo($snackList);
+        $(`<h4 class="snack-name">`).text(snack.name).appendTo($snackList);
+        //$(`<p class="snack-description">`).text(snack.description).appendTo($snackList);
         $(`<p class="snack-price">`).text(`Item Price: $${(snack.price)/100}`).appendTo($snackList);
-
       }
     });
   }
@@ -70,11 +70,11 @@ $(document).ready(function () {
       $drinkList.empty();
 
       for(const drink of response.items) {
-        $(`<h4 class="drink-name">`).text(drink.name).appendTo($drinkList);
-        $(`<p class="drink-description">`).text(drink.description).appendTo($drinkList);
         $(`<img src=${drink.img_url} class="item-image">`).appendTo($drinkList);
+        $(`<i class="fa-solid fa-cart-plus"></i>`).appendTo($drinkList);
+        $(`<h4 class="drink-name">`).text(drink.name).appendTo($drinkList);
+        //$(`<p class="drink-description">`).text(drink.description).appendTo($drinkList);
         $(`<p class="drink-price">`).text(`Item Price: $${(drink.price)/100}`).appendTo($drinkList);
-
       }
     console.log(response.items)
     });
