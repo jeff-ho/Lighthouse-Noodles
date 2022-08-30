10685a57f2f3a65111e33536e4f182fe43c8266a
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-30 16:16:39 +0000

    Checkout page totals with taxes

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,32 +1,35 @@
 $(document).ready(function () {
+  const renderCheckout = function () {
+    $.ajax({
+      method: "GET",
+      url: "/api/checkout",
+    }).done((response) => {
+      console.log(response, "response");
+      const $checkoutList = $(".items-checkout");
+      $checkoutList.empty();
+      let sum = 0;
+      let userName = response.items[0];
+      console.log(userName);
 
-    const renderCheckout = function() {
-      $.ajax({
-        method: 'GET',
-        url: '/api/checkout'
-      })
-      .done((response) => {
-        console.log(response, 'response')
-        const $checkoutList = $('.items-checkout');
-        $checkoutList.empty();
-        let sum = 0;
-
-        for(const checkout of response.items) {
-          $(
-            `
+      for (const checkout of response.items) {
+        $(
+          `
             <div class="item">
             <h4>${checkout.name}</h4>
             <div><span>Quantity: ${checkout.quantity}</span>
-            <span>Price: $${(checkout.price)/100 * checkout.quantity}</span>
+            <span>Price: $${(checkout.price / 100) * checkout.quantity}</span>
              </div>
             </div>
             `
-          ).appendTo($checkoutList);
-            sum+= checkout.price
-        }
-        $(`<p>Subtotal: $${sum/100}</p>`).appendTo(`#subtotal`)
-      });
-    }
-    renderCheckout();
-
-})
+        ).appendTo($checkoutList);
+        sum += checkout.price;
+      }
+      let taxes = (sum / 100) * 0.13;
+      let subtotal = sum / 100;
+      $(`<p>Subtotal: $${sum / 100}</p>
+        <p>Taxes: $${taxes}</p>
+        <p>Total: $${(subtotal + taxes).toFixed(2)}</p>`).appendTo(`#subtotal`);
+    });
+  };
+  renderCheckout();
+});
