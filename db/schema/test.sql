15e02a924795aac05426de88b21a3a7e82ddd6f1
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-08-30 17:51:22 -0400

    Add header.ejs and make navbar appears in all pages

--- db/schema/test.sql
+++ db/schema/test.sql
@@ -1,6 +1,14 @@
-SELECT user_id, users.name, item_id, items.name as name, SUM(item_quantity) as quantity, items.price
+SELECT carts.id, user_id, users.name as username, item_id, items.name as name, SUM(item_quantity) as quantity, items.price as price
 FROM carts
 JOIN items ON item_id = items.id
 JOIN users ON user_id = users.id
-GROUP BY user_id, item_id, items.price, items.name, users.name
+GROUP BY items.price, items.name, users.name, carts.id
+ORDER BY item_id;
+
+
+SELECT carts.id, items.img_url as img, user_id, users.name, item_id, items.name as name, SUM(item_quantity) as quantity, items.price as price
+FROM carts
+JOIN items ON item_id = items.id
+JOIN users ON user_id = users.id
+GROUP BY user_id, item_id, items.price, items.img_url, items.name, users.name,carts.id
 ORDER BY item_id;
