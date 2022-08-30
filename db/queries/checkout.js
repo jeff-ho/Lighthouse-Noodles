e24815ddc4ce9afc5d5b8ab45748c8e9fdc1bd85
Jeffrey <jeffrey-lee@live.ca>
2022-08-30 17:27:52 +0000

    HTML for checkout

--- db/queries/checkout.js
+++ db/queries/checkout.js
@@ -2,7 +2,7 @@ const db = require('../connection');
 
 const addToCheckout = (item) => {
   return db.query(
-    'SELECT user_id, users.name, item_id, items.name as name, SUM(item_quantity) as quantity, items.price FROM carts JOIN items ON item_id = items.id JOIN users ON user_id = users.id GROUP BY user_id, item_id, items.price, items.name, users.name ORDER BY item_id;')
+    'SELECT items.img_url as img, user_id, users.name, item_id, items.name as name, SUM(item_quantity) as quantity, items.price FROM carts JOIN items ON item_id = items.id JOIN users ON user_id = users.id GROUP BY user_id, item_id, items.price, img, items.name, users.name ORDER BY item_id;')
     .then(data => {
       return data.rows;
     });
