a39ec328c0a0e5afffa44c46d87f63132c5c6bb7
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-31 17:37:29 +0000

    Implement delete cart item function

--- routes/twilio-api.js
+++ routes/twilio-api.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router  = express.Router();
 const sendText = require('../sms');
 
+
+
 router.post('/', (req, res) => {
-  let totalPrice = req.body.total;
- sendText.sendText(totalPrice)
+let totalPrice = req.body.total;
+sendText.sendTextToUser(totalPrice)
 });
 
 module.exports = router;
